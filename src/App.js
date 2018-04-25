79513e6f531b583ceba80b345e4e4a8d2cd7336c
jerrybennett <jerrysbusiness@gmail.com>
2018-04-25 15:55:46 -0400

    test fetch with axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import IdeasContainer from './IdeasContainer'
+
+import { connect } from 'react-redux';
+
+import IdeasContainer from './IdeasContainer';
 
 class App extends Component {
   render() {
+    const { fetching, dog, onRequestDog, error } = this.props
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Idea Board</h1>
+          {dog.map(d => <img src={d || logo} className='App-logo' alt='logo' />)}
+          <h1 className="App-title">Welcome to Dog Saga</h1>
+          {/* <h1 className="App-title">Idea Board</h1> */}
         </header>
         <IdeasContainer />
+        {dog ? (
+          <p className="App-intro">Keep clicking for new dogs</p>
+        ) : (
+          <p className="App-intro">Replace the React icon with a dog!</p>
+        )}
+
+        {fetching ? (
+          <button disabled>Fetching...</button>
+        ) : (
+          <button onClick={onRequestDog}>Request a Dog</button>
+        )}
+
+        {error && <p style={{ color: 'red' }}>Uh oh - something went wrong!</p>}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    fetching: state.fetching,
+    dog: state.dog,
+    error: state.error
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onRequestDog: () => dispatch({
+      type: 'API_CALL_REQUEST'
+    })
+  };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
