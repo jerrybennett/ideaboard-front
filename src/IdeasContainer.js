5c3fe7b81bfb22c9bdbbf6794aacc90f7475d800
jerrybennett <jerrysbusiness@gmail.com>
2018-04-25 13:10:20 -0400

    update css, add component, install redux-saga

---
+++ src/IdeasContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class IdeasContainer extends Component {
+  state = {
+    ideas: []
+  }
+  render() {
+    return (
+      <div>
+        Ideas
+      </div>
+    );
+  }
+}
